2bbd5dbf210032e53849015f5d110a7de784ad18
Helen King <helenking029@gmail.com>
2016-11-02 23:03:14 -0700

    breaking down steps for js functions

--- hike.js
+++ hike.js
@@ -10,3 +10,16 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+
+// User selects hike or adventure from a set of radial? buttons
+
+
+//return list of foods or meal options suitable for that adventure
+
+
+// return feedback, social media info about that meal, food item
+
+
+// User Story  - have a slider image that shows different users experiences or stories?
