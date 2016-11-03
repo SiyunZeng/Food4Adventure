3963848273af7b45157dad335115d7ce640a88b8
Helen King <helenking029@gmail.com>
2016-11-02 20:49:21 -0700

    Adding navigation bar

--- hike.js
+++ hike.js
@@ -1,1 +1,12 @@
 //javascript goes here
+
+
+// Navigation Bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
