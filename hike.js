3c45e0fbf83989b0cbe84c2a5d2a0af173c73a9c
helenking029 <helenking029@gmail.com>
2016-11-28 20:18:34 -0800

    cleaned up the index.html file

--- hike.js
+++ hike.js
@@ -13,9 +13,24 @@ function myFunction() {
 
 
 
-// User selects hike or adventure from a set of radial? buttons
 
 
+// User selects hike or adventure from a set of radial? buttons - calories are displayed
+function comox() {
+    alert("You need 4000 calories for this hike!");
+}
+function albert() {
+    alert("You need 3500 calories for this hike!");
+}
+function kludahk() {
+    alert("You need 4500 calories for this hike!");
+}
+function westcoast() {
+    alert("You need 6000 calories for this hike!");
+}
+function juandefuca() {
+    alert("You need 5000 calories for this hike!");
+}
 //return list of foods or meal options suitable for that adventure
 
 
