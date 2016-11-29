80fc69ef815bfa053709315912d76115d09afd93
Philip Bazzard <babarackas@gmail.com>
2016-11-29 13:09:14 -0800

    connected to the realtime database with Hello World doesn't work with all of the JSON object however but this might inspire someone with a solution

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-(function)() {
-   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBr6iKw0flJw28mStxf_clP2_ZBIQTORso",
-    authDomain: "food4adventure.firebaseapp.com",
-    databaseURL: "https://food4adventure.firebaseio.com",
-    storageBucket: "food4adventure.appspot.com",
-    messagingSenderId: "1047852701386"
-  };
-  firebase.initializeApp(config);  
 
 
-//Get Elements
-const preObject = document.getElementById('object');
 
-// Create references
-const dbRefObject = firebase.database().ref().child('object');
-
-//Sync object changes
-dbRefObject.on('value', snap => {
-    preObject.innerText = JSON.stringify(snap.val(), null, 3);
-});
-
-
-}());
