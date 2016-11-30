fb22e0ea525947499176ddb076378397a6bfa935
Philip Bazzard <babarackas@gmail.com>
2016-11-30 11:24:03 -0800

    added realtime DB concept doesn't do much but if you change info in Firebase it does update the website

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 
 
 
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBr6iKw0flJw28mStxf_clP2_ZBIQTORso",
+    authDomain: "food4adventure.firebaseapp.com",
+    databaseURL: "https://food4adventure.firebaseio.com",
+    storageBucket: "food4adventure.appspot.com",
+    messagingSenderId: "1047852701386"
+  };
+  firebase.initializeApp(config);
+
+  var bigOne = document.getElementById('bigOne');
+  var dbRef = firebase.database().ref().child('users');
+  var dbRef2 = firebase.database().ref().child('calories');
+  
+dbRef2.on('value', snap => calories.innerText = snap.val());
